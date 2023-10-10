89d2c26a219791fb4788184d7dcacba199f6bf1d
TonzN <66659994+TonzN@users.noreply.github.com>
2023-10-11 00:10:24 +0200

    Create driver.py

---
+++ driver.py
@@ -0,0 +1,104 @@
+import ui
+import chess_lib as cl
+import chess
+#DEMO
+
+window = ui.NewWindow()
+window_size = (480,480)
+window.reSizeScreen(window_size)
+window.Target_fps = 60
+
+screen = window.screen
+
+board = chess.Board()
+
+grid = ui.grid(window_size, 60)
+grid.generate(screen)
+
+game = cl.Game(screen)
+game.setup(grid.grid, 60, True)
+game.loadPawns()
+game.loadPieces()
+
+select_layer = ui.RenderQueue()
+
+run = True
+selected_piece = None
+new_turn = False
+
+#backend game vars
+move = None
+
+while run:
+    window.NextFrame([select_layer])
+    
+    if window.leftclick() == True: #Select
+        if selected_piece: 
+            new_uci_pos = game.grid_pos_to_uci(game.round_to_grid(window.mousepos))
+            new_pos = game.Positions[new_uci_pos]
+            is_legal_move = False
+            #Check if the move is legal
+            legal_moves = list(board.legal_moves)
+            for i in legal_moves:
+                if selected_piece+new_uci_pos == str(i):
+                    is_legal_move = True
+            
+            if is_legal_move == True: 
+                print(selected_piece+new_uci_pos)
+                move = selected_piece+new_uci_pos  #assigns move   
+                uci_move = chess.Move.from_uci(move)    
+            
+                #capture    
+                if board.piece_at(uci_move.to_square):
+                    ui.MainRenderQueue.Remove(game.pieces[new_uci_pos].img)
+                    del game.pieces[new_uci_pos]
+                
+                #en passant
+                if board.is_en_passant(uci_move):
+                    ui.MainRenderQueue.Remove(game.pieces[game.last_move].img)
+                
+                #castle
+                if board.is_castling(uci_move):
+                    new_file = chess.square_file(uci_move.to_square)
+                    curr_file = chess.square_file(uci_move.from_square)
+                    #castling dir
+                    print("is castling")
+                    if new_file > curr_file: #kingside
+                        if board.turn:
+                            game.move("h1f1")
+                        else:
+                            game.move("h8f8")
+                    else: #queenside
+                        if board.turn:
+                            game.move("a1d1")
+                        else:
+                            game.move("a8d8")
+                
+                #moves piece
+                game.pieces[selected_piece].img.pos = new_pos   
+                game.pieces[new_uci_pos] = game.pieces[selected_piece]    
+                del game.pieces[selected_piece] 
+                selected_piece = None
+                select_layer.Queue = []
+                ui.MainRenderQueue.Push(game.pieces[new_uci_pos].img) 
+                game.last_move = new_uci_pos
+            
+        else:
+            selected_piece = game.select_piece(window.mousepos)
+    
+    if window.rightclick() == True and selected_piece: #Deselect
+        #Snapback piece to original position
+        game.move_to_original_pos(selected_piece)
+        select_layer.Pop()
+        ui.MainRenderQueue.Push(game.pieces[selected_piece].img)
+        
+        selected_piece = None
+         
+    if selected_piece:
+        if ui.LinearSearch(ui.MainRenderQueue.Queue, game.pieces[selected_piece].img):
+            ui.MainRenderQueue.Remove(game.pieces[selected_piece].img)
+            select_layer.Push(game.pieces[selected_piece].img)
+        
+        game.follow_cursor(selected_piece, window.mousepos)
+        
+    if move: #BACKEND
