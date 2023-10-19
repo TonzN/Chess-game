24bf305eef48fb40228e6700ba07c3679aadf864
TonzN <66659994+TonzN@users.noreply.github.com>
2023-10-20 01:32:21 +0200

    Update driver.py

--- driver.py
+++ driver.py
@@ -4,6 +4,7 @@ import chess
 import engine
 import time
 import opening_database
+import timeit
 #DEMO
 
 window = ui.NewWindow()
@@ -23,6 +24,7 @@ game.setup(grid.grid, 60, False)
 game.loadPawns()
 game.loadPieces()
 engine.game_vars["debug_move"] = False
+engine.game_vars["game"] = game
 
 select_layer = ui.RenderQueue()
 
@@ -34,6 +36,7 @@ new_turn = False
 move = None
 player = True
 promotion = False
+enable_eval = True
 
 promotion_table = {
     "queen": "q",
@@ -46,8 +49,14 @@ selected_promotion = False
 while run:
     window.NextFrame([select_layer])
     if not player == board.turn:
-        move = engine.run(game.last_move_uci)
+        move = engine.run(game.last_move_uci, board)
+        
         if move:
+            if board.is_check():
+                engine.game_vars["is_check"] = True
+            else:
+                engine.game_vars["is_check"] = False
+                
             uci_move = chess.Move.from_uci(move.move)
             game.capture(board, uci_move, move.move[2:])
             game.castle(board, uci_move)
@@ -56,8 +65,11 @@ while run:
             game.last_move = move.move[:2]
             
             board.push(uci_move)
-            game.last_move_uci = uci_move
+            engine.played_moves.append(uci_move)
+            game.last_move_uci = uci_move   
             move = None
+            engine.pre_compute(board)
+            print("\nEngine eval", engine.eval(board), "Color:", board.turn)
     
     if window.leftclick() == True: #Select
         if selected_piece: 
@@ -127,32 +139,40 @@ while run:
             team = "black"
         promotion_move = chess.Move.from_uci(game.last_move_uci+selected_promotion[0]) 
         board.push(promotion_move) 
+        engine.played_moves.append(promotion_move)
         game.pieces[game.last_move] =  cl.Piece(team, selected_promotion[1].piece_type, game.last_move, game.Positions[game.last_move], screen)
         selected_promotion = None
         promotion = None
         move = None
         selected_piece = None
         select_layer.Queue = []
-
-        
-    if move and not promotion: #BACKEND
+      
+    if move and not promotion: #BACKEND 
         uci_move = chess.Move.from_uci(move)
         board.push(uci_move)
+        engine.played_moves.append(uci_move)
         move = None
+        
         if game.debugMode:
             print(board)
-        
+    
         white_material = engine.white_material
         black_material = engine.black_material
         print("\nWhite captured material {white} | Black captured material {black}".format(white = white_material, black = black_material))
         
         if white_material > black_material:
-            print("\n White: +{advantage}".format(advantage=white_material-black_material))
+            print("\nWhite: +{advantage}".format(advantage=white_material-black_material))
         elif black_material > white_material:
-            print("\n Black: +{advantage}".format(advantage=black_material-white_material))
+            print("\nBlack: +{advantage}".format(advantage=black_material-white_material))
         
         print("\nWhite captured pieces: ", engine.white_captured_pieces)
         print("Black captured pieces: ", engine.black_captured_pieces)
+        
+        if enable_eval:
+            team_legal_moves = []
+            engine.pre_compute(board)
+            print("\nPlayer eval:", engine.eval(board), "Color:",board.turn)
+           
         if board.is_repetition():
             print("repetition")
             ui.endPygame()
