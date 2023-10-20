28d93edf6243b08b770554130d2b8e156066fa72
TonzN <66659994+TonzN@users.noreply.github.com>
2023-10-20 17:07:11 +0200

    Update opening_database.py

--- opening_database.py
+++ opening_database.py
@@ -16,11 +16,12 @@ class Node:
             child.print_tree(depth + 1)
 
 class Tree:
-    def __init__(self, move):
+    def __init__(self, name, move):
         self.root = Node(move)
-
+        self.name = name
+        
 ##################################||||RUY LOPEZ||||#############################################33
-Ruy_lopez = Tree("e2e4")
+Ruy_lopez = Tree("Ruy Lopez", "e2e4")
 Ruy_lopez.root.children["e2e4"] = Node("e7e5")
 Ruy_lopez.root.children["e2e4"].children["e7e5"] = Node("g1f3", "Kn")
 Ruy_lopez.root.children["e2e4"].children["e7e5"].children["g1f3"] = Node("b8c6", "Kn")
@@ -28,18 +29,20 @@ Ruy_lopez.root.children["e2e4"].children["e7e5"].children["g1f3"].children["b8c6
 Ruy_lopez_position = Ruy_lopez.root.children["e2e4"].children["e7e5"].children["g1f3"].children["b8c6"] 
 
 ##################################||||Vienna game||||#############################################
-vienna_game = Tree("e2e4")
+vienna_game = Tree("Vienna game", "e2e4")
 vienna_game.root.children["e2e4"] = Node("e7e5")
 vienna_game.root.children["e2e4"].children["e7e5"] = Node("b1c3", "Kn")
 vienna_game.root.children["e2e4"].children["e7e5"].children["b1c3"] = Node("g8f6", "Kn")
 vienna_game.root.children["e2e4"].children["e7e5"].children["b1c3"].children["g8f6"] = Node("f2f4")
 vienna_game.root.children["e2e4"].children["e7e5"].children["b1c3"].children["g8f6"].children["f2f4"] = Node("d7d5")
-vienna_game = vienna_game.root.children["e2e4"].children["e7e5"].children["b1c3"].children["g8f6"]
+vienna_game_position = vienna_game.root.children["e2e4"].children["e7e5"].children["b1c3"].children["g8f6"]
 
 ################################||||Queens gambit||||###############################################
-queens_gambit = Tree("d2d4")
+queens_gambit = Tree("Queens Gambit", "d2d4")
 queens_gambit.root.children["d2d4"] = Node("d7d5")
 queens_gambit.root.children["d2d4"].children["d7d5"] = Node("c2c4")
-#queens_gambit.root.children["e2e4"].children["e7e5"].children["b1c3"] = Node("g8f6", "Kn")
+queens_gambit_position = queens_gambit.root.children["d2d4"].children["d7d5"]
 
-queens_gambit = queens_gambit.root.children["d2d4"].children["d7d5"]
+################################||||Queens gambit accepted||||###############################################
+queens_gambit_accepted_position = queens_gambit.root.children["d2d4"].children["d7d5"]
+queens_gambit_accepted_position.children["c2c4"] = Node("d5c4")
