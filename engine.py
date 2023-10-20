26fa2a3c266d5cc1c6e8464cfd9ce367c02233c6
TonzN <66659994+TonzN@users.noreply.github.com>
2023-10-20 16:50:37 +0200

    Update engine.py

--- engine.py
+++ engine.py
@@ -3,7 +3,8 @@ import chess
 import opening_database
 import time
 import timeit
-import random
+import chess.polyglot
+import random as rand
 
 engine_piece_values = {
     "p": 1,
@@ -23,6 +24,7 @@ piece_values = {
 }
 
 game_vars = {
+    "board": False,
     "in_opening": True,
     "in_opening_book": True,
     "opening_move": None,
@@ -30,7 +32,10 @@ game_vars = {
     "debug_mode": False,
     "game": None,
     "is_check": False,
-    "pieces": [[],[]]
+    "pieces": [[],[]],
+    "check_mate": False,
+    "start_depth": 4,
+    "positions_ran": 0
 }
 
 center = {
@@ -40,6 +45,7 @@ center = {
     "e5": 0.05,
 }
 
+attacking_squares = {}
 center_pawns_white = {}
 center_pawns_black = {}
 legal_piece_moves_white = {
@@ -58,6 +64,8 @@ legal_piece_moves_black = {
     2: [],
     1: []
 }
+legal_moves_white = {-1: [], 0: [], 1: [], 2: [], 3: [], 4:[], 5: []}
+legal_moves_black = {-1: [], 0: [], 1: [], 2: [], 3: [], 4:[], 5: []}
 
 opening_moves = []
 played_moves = []
@@ -150,7 +158,7 @@ def main_opening_search(last_move):
         for i in opening_moves:
             if i in next_move.children:
                 if len(next_move.children) > 1:
-                    next_move = next_move.children[random.choice(next_move.children)] #random opening variation
+                    next_move = next_move.children[rand.choice(next_move.children)] #random opening variation
                 else:
                     next_move = next_move.children[i]
                     selected_move = next_move
@@ -168,7 +176,7 @@ def main_opening_search(last_move):
     if len(available_moves) < 1: #didnt find any opening move
         return
     
-    selected_move = random.choice(available_moves)
+    selected_move = rand.choice(available_moves)
     
     if selected_move: #double check so it doesnt select a move already played.
         if selected_move.move == last_move:
@@ -192,36 +200,42 @@ def material_count(board, team): #returns material for chosen team
                 
     return material
 
-def pre_compute(board):
+def pre_compute(board, depth=0):
     for i in legal_piece_moves_white:
         legal_piece_moves_white[i] = []
-        legal_piece_moves_black[i] = []
+        legal_piece_moves_black[i] = [] 
     
     original_turn = board.turn
     board.turn = True
     center_pawns_black = {}
     center_pawns_white = {}
-    
+    legal_moves_white[depth] = []
+    legal_moves_black[depth] = []
     for move in list(board.legal_moves):
         piece = board.piece_at(move.from_square)
         move_uci = move.uci()
         legal_piece_moves_white[piece.piece_type].append(move_uci)
+        legal_moves_white[depth].append(move)
+        if not move_uci[:2] in attacking_squares:
+            attacking_squares[move_uci[:2]] = move_uci[:2]
         if piece.piece_type == 1:
             if move_uci[:2] == "e4" or "e5" or "d5" or "d4":
                 center_pawns_white[move_uci[:2]] = move_uci[:2]
-
+    
     board.turn = False
     for move in list(board.legal_moves):
         piece = board.piece_at(move.from_square)
         move_uci = move.uci()
         legal_piece_moves_black[piece.piece_type].append(move_uci)
+        legal_moves_black[depth].append(move)
+        if not move_uci[:2] in attacking_squares:
+            attacking_squares[move_uci[:2]] = move_uci[:2]
         if piece.piece_type == 1:
             if move_uci[:2] == "e4" or "e5" or "d5" or "d4":
                 center_pawns_black[move_uci[:2]] = move_uci[:2]
     
     board.turn = original_turn
-        
-    
+           
     #precomputes gamevars
     game_vars["pieces"] = [[],[]]
     for square, piece in board.piece_map().items():
@@ -230,7 +244,7 @@ def pre_compute(board):
         else:
             game_vars["pieces"][1].append(piece) 
     
-def mobility(board, team):
+def mobility(board, team, depth):
     mobility_score = 0
     legal_moves = legal_piece_moves_white
     center_pawn = center_pawns_white
@@ -249,7 +263,6 @@ def mobility(board, team):
             mobility_score += knight_preffered_positions[move[:2]]
             if move[2:] in center:
                 mobility_score += center[move[2:]]
-            
     mobility_score += 0.03 * len(legal_moves[3])
     if game_vars["in_opening"] == False:
         mobility_score += 0.035 * len(legal_moves[4])
@@ -259,25 +272,28 @@ def mobility(board, team):
     
     return mobility_score
         
-def eval(board):
+def eval(board, depth=0):
     #get scores
     original_turn = board.turn
-    len_pieces = len(game_vars["game"].pieces)
+    piece_map = board.piece_map()
+    len_pieces = len(piece_map)
+     
     material_white = 0
     material_black = 0
     
-    #cached material 
-    if not len_pieces in cached_material_white  or not len_pieces in cached_material_black:
+    #cached material }
+    if not len_pieces in cached_material_white or not len_pieces in cached_material_black:
         material_white = material_count(board, True)
         material_black = material_count(board, False)
         cached_material_white[len_pieces] = material_white
         cached_material_black[len_pieces] = material_black
+        print(material_white-material_black)
     else:
-        material_white = cached_material_white[len_pieces]
-        material_black = cached_material_black[len_pieces]
-     
-    mobility_white = mobility(board,True)
-    mobility_black = mobility(board, False)
+        material_white = material_count(board, True)
+        material_black = material_count(board, False)
+      
+    mobility_white = mobility(board,True, depth)
+    mobility_black = mobility(board, False, depth)
     
     board.turn = original_turn
     
@@ -292,6 +308,59 @@ def eval(board):
     Eval = (material_score+mobility_score)*who_2_move
 
     return Eval
+# Initialize Zobrist hashing keys
+fakeboard = chess.Board()
+zobrist_keys = { piece: [[chess.polyglot.zobrist_hash(fakeboard) for _ in range(64)] for _ in range(2)]
+    for piece in chess.PIECE_TYPES
+}
+
+transposition_table = {}
+
+def calculate_zobrist_key(board):
+    key = 0
+    for square, piece in board.piece_map().items():
+        if piece is not None:
+            color = int(piece.color)
+            key ^= zobrist_keys[piece.piece_type][color][square]
+    return key
+
+def negamax(board, depth, alpha, beta):
+    key = calculate_zobrist_key(board)
+    game_vars["positions_ran"] += 1
+    if key in transposition_table:
+        if transposition_table[key][2] >= depth:
+           pass      
+          #  return transposition_table[key]
+    
+    if depth == 0:
+        return [eval(board, depth), None]
+
+    legal_moves = legal_moves_white[game_vars["start_depth"]-depth]
+    if board.turn == False:
+        legal_moves = legal_moves_black[game_vars["start_depth"]-depth]
+        
+    best_move = None
+   
+    pre_compute(board, game_vars["start_depth"]-depth)
+        
+    legal_moves = legal_moves_white[game_vars["start_depth"]-depth]
+    if board.turn == False:
+        legal_moves = legal_moves_black[game_vars["start_depth"]-depth]
+        
+    for move in legal_moves:
+        board.push(move)
+        score = -negamax(board, depth - 1, -beta, -alpha)[0]
+        board.pop()
+
+        if score >= beta:
+            return [score, move]  # Beta cutoff
+        if score > alpha:
+            alpha = score  # New best move
+            best_move = move
+    
+    transposition_table[key] = [alpha, best_move, depth]
+
+    return [alpha, best_move]
 
 def run(last_move, board):
     move = None
@@ -302,18 +371,31 @@ def run(last_move, board):
         game_vars["in_opening"] = False
         
     if game_vars["in_opening_book"]:
-        move = main_opening_search(last_move)
+        book_move = main_opening_search(last_move)
+        if book_move:
+            move = book_move.move
         if not move:
             game_vars["in_opening_book"] = False
+    else:
+        time_now = time.perf_counter()
+        search_data = negamax(board, 4, float('-inf'), float('inf'))
+        time_new = time.perf_counter()
+        print("time spent: ", time_new-time_now)
+        print("Searched positions: ", game_vars["positions_ran"])
+        game_vars["positions_ran"] = 0
+        print(search_data)
+        move = str(search_data[1].uci())
         
     def code_to_time():
-        #pre_compute(board)
-        eval(board)
-         
-    time_taken = timeit.timeit(code_to_time, number=1)
+        pre_compute(board)
+        #eval(board, -1)
+        #check()    
+    
+    time_taken = timeit.timeit(code_to_time, number=100 )
     print("Elapsed time:", time_taken, "seconds")
-    eval_score = eval(board) *-1 #no move has been pushed, flips
+    eval(board) *-1 #no move has been pushed, flips
 
     #start engine
             
     return move#Returns a move
+
